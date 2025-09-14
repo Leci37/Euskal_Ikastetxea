ab9665c0deaa8f223205864ca075858278b39ef1
Luis Le <110530848+Leci37@users.noreply.github.com>
2025-09-14 17:23:50 +0200

    Add quiz trigger from NPC

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import GameEngine from './src/core/GameEngine.js';
 import SceneManager from './src/scenes/SceneManager.js';
 import OverworldScene from './src/scenes/OverworldScene.js';
+import VocabularyQuizScene from './src/scenes/VocabularyQuizScene.js';
 
 const game = new GameEngine();
 SceneManager.registerScene('Overworld', OverworldScene);
+SceneManager.registerScene('VocabularyQuizScene', VocabularyQuizScene);
 
 const assetManifest = {};
 
