750715f92e3df09bde61f1b461679282b8d4fd5c
Luis Le <110530848+Leci37@users.noreply.github.com>
2025-09-14 16:23:10 +0200

    feat: add Overworld scene

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import GameEngine from './src/core/GameEngine.js';
+import SceneManager from './src/scenes/SceneManager.js';
+import OverworldScene from './src/scenes/OverworldScene.js';
 
 const game = new GameEngine();
+SceneManager.registerScene('Overworld', OverworldScene);
 
 const assetManifest = {};
 
 async function main() {
   await game.start(assetManifest);
+  SceneManager.switchTo('Overworld');
 }
 
 main();
